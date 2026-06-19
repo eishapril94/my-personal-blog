a224685c2d9332d5ed9edcf0a4ffea4f3ebe9f1f
kyanij <mkyanij1@gmail.com>
2026-06-20 01:21:51 +0545

    changed config of frontend to redirect to admin.

--- frontend/js/config.js
+++ frontend/js/config.js
@@ -1,5 +1,7 @@
 const APP_CONFIG = {
-  API_BASE_URL: 'http://localhost:1337',
+  // API_BASE_URL: 'http://localhost:1337',
+    API_BASE_URL: 'https://my-personal-blog-areb.onrender.com',
+
 };
 
 document.addEventListener('DOMContentLoaded', () => {
